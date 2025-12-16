8e0e5699a8c480c5494df061dd192a155dfebf0d
Ahsan <195335988+ahsand871@users.noreply.github.com>
2025-12-16 06:56:53 +0500

    update

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -39,7 +39,7 @@ export default function TextForm(props) {
         props.setText(newText);
     }
     const handleDemoText = () =>{
-        let newText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus in tenetur voluptates ex excepturi rem. Possimus vel repellendus maiores labore iusto et expedita, distinctio, nemo sapiente omnis, atque ea deleniti blanditiis vero maxime! Quasi dolores corrupti, officia deserunt magnam voluptatum autem sequi eligendi aliquam fugit repudiandae dicta illum dolorem cupiditate?';
+        let newText = `${props.text} Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus in tenetur voluptates ex excepturi rem. Possimus vel repellendus maiores labore iusto et expedita, distinctio, nemo sapiente omnis, atque ea deleniti blanditiis vero maxime! Quasi dolores corrupti, officia deserunt magnam voluptatum autem sequi eligendi aliquam fugit repudiandae dicta illum dolorem cupiditate?`;
         props.setText(newText);
     }
     const handleOnChange = (event) => {
