c357673f90485f0417c20700ba031327dc87ff45
Ahsan <195335988+ahsand871@users.noreply.github.com>
2025-12-16 06:27:22 +0500

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props) {
         <nav className={props.isDark ? 'navbar navbar-expand-lg bg-body-dark bg-dark' : 'navbar navbar-expand-lg bg-body-primary bg-primary'}
                 data-bs-theme={props.isDark ? 'dark' : 'light'}>
          <div className="container-fluid">
-             <a className="navbar-brand" href="#" >Ahsan's Blogs</a>
+             <a className="navbar-brand" href="#" >Ahsan's Text Editor</a>
              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
              <span className="navbar-toggler-icon"></span>
              </button>
@@ -14,20 +14,7 @@ export default function Navbar(props) {
                  <li className="nav-item">
                  <a className="nav-link active" aria-current="page" href="#">Home</a>
                  </li>
-                 <li className="nav-item">
-                 <a className="nav-link" href="#">{props.writerName}</a>
-                 </li>
-                 <li className="nav-item dropdown">
-                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                     Category
-                 </a>
-                 <ul className="dropdown-menu">
-                     <li><a className="dropdown-item" href="#">Nature</a></li>
-                     <li><a className="dropdown-item" href="#">Lifestyle</a></li>
-                     <li><a className="dropdown-item" href="#">Informational</a></li>
-                 </ul>
-                 </li>
-             </ul>
+            </ul>
 
              <form className="d-flex" role="search">
                  <input className="form-control me-2" type="search" style={props.isDark? {background:'#121212'}: {background:'white'}} placeholder="Search" aria-label="Search"/>
